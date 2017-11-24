ad1b23837b81bf56c8d0d9b5070e51a9ce076aab
Olberg Egil <olbesegi@ul001616.eu.tieto.com>
2017-11-24 15:03:50 +0100

    Initial version

---
+++ src/main/java/org/olberg/App.java
@@ -0,0 +1,13 @@
+package org.olberg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
